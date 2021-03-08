d8cccb190b2bf23828503d48d23225544575069c
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-03-08 09:37:27 +0700

    create SimpleSingleton class

---
+++ Singleton/SimpleSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class SimpleSingleton
+    {
+        private SimpleSingleton()
+        {
+            
+        }
+        private static SimpleSingleton instance;
+        public static SimpleSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SimpleSingleton();
+            }
+            return instance;
+        }
+    }
+}
